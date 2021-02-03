249ec7c17c5d8638a0ac1986903e3fbc72a06bf9
diddy032 <gwul3vu032@gmail.com>
2021-02-04 00:37:21 +0800

    add styles file

--- components/Clock/index.js
+++ components/Clock/index.js
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import styles from "./styles/Clock.module.sass";
 export default function Indexs() {
-  return <div>時鐘的Ｃomponent</div>;
+  return (
+    <div className={styles["clock-wrap"]}>
+      <div className={styles["logo-bg"]}>
+        <h1>PODOMORO</h1>
+      </div>
+
+      <div className={styles["content-text"]}>
+        You don't have any task now. <br />
+        please add task first!
+      </div>
+      <div className={styles["footer-text"]}>PODOMORO</div>
+    </div>
+  );
 }
