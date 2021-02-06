ce8b010fc13c7afd631c8f78bd9d8de5b9627c56
diddy032 <gwul3vu032@gmail.com>
2021-02-06 15:34:58 +0800

    navbar 切版

--- components/AddTask/index.js
+++ components/AddTask/index.js
@@ -1,3 +1,15 @@
+import styles from "./styles/AddTask.module.sass";
 export default function Indexs() {
-  return <div>增加項目的Ｃomponent</div>
+  return (
+    <div id={styles["add-task"]}>
+      <div className={styles["title"]}>ADD NEW TASK</div>
+      <label for="input-task" htmlFor="">
+        TASK TITLE
+      </label>
+      <input id="input-task" type="text" placeholder="My Task" />
+      <p>ESTIMATED TOMOTO</p>
+
+      <button>ADD TASK</button>
+    </div>
+  );
 }
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,53 +1,71 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import AddTask from "../AddTask";
+import TaskList from "../TaskList";
+import Analutics from "../Analytics";
+import RingTone from "../RingTone";
 import styles from "./styles/Navbar.module.sass";
 export default function Indexs() {
   const [openNavbar, setOpenNavbar] = useState(false);
+  console.log("openNavbar:", openNavbar);
   return (
-    <div id={styles["navbar"]}>
-      <div className={styles["nav-list"]}>
-        <button>
-          <Image
-            src="/images/icons/add_white.svg"
-            alt=""
-            width={80}
-            height={80}
-          />
-        </button>
-        <button>
-          <Image
-            src="/images/icons/list_white.svg"
-            alt=""
-            width={80}
-            height={80}
-          />
-        </button>
-        <button>
-          <Image
-            src="/images/icons/analysis_white.svg"
-            alt=""
-            width={80}
-            height={80}
-          />
-        </button>
-        <button>
-          <Image
-            src="/images/icons/ringtone_white.svg"
-            alt=""
-            width={80}
-            height={80}
-          />
-        </button>
+    <>
+      <div
+        id={styles["navbar"]}
+        className={openNavbar ? styles["open-navbar"] : null}
+      >
+        <div className={styles["nav-list"]}>
+          <button>
+            <Image
+              src="/images/icons/add_white.svg"
+              alt="增加"
+              width={80}
+              height={80}
+            />
+          </button>
+          <button>
+            <Image
+              src="/images/icons/list_white.svg"
+              alt="列表"
+              width={80}
+              height={80}
+            />
+          </button>
+          <button>
+            <Image
+              src="/images/icons/analysis_white.svg"
+              alt="分析"
+              width={80}
+              height={80}
+            />
+          </button>
+          <button>
+            <Image
+              src="/images/icons/ringtone_white.svg"
+              alt="鈴聲"
+              width={80}
+              height={80}
+            />
+          </button>
+          <button
+            className={styles["nav-button"]}
+            onClick={() => setOpenNavbar(!openNavbar)}
+          >
+            <Image
+              src="/images/icons/tomato_small_color.svg"
+              alt="小番茄"
+              width={25}
+              height={25}
+            />
+            <img
+              className={styles["icon-arrow"]}
+              src="/images/icons/arrow.svg"
+              alt="箭頭"
+            />
+          </button>
+        </div>
+        {openNavbar ? <AddTask /> : null}
       </div>
-      <button className={styles["nav-button"]}>
-        <Image
-          src="/images/icons/tomato_small_color.svg"
-          alt=""
-          width={25}
-          height={25}
-        />
-        <Image src="/images/icons/arrow.svg" alt="" width={25} height={25} />
-      </button>
-    </div>
+    </>
   );
 }
