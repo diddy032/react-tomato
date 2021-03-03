41347992f5bda015559acafa6d6a9a3999cddffd
diddy032 <gwul3vu032@gmail.com>
2021-03-03 23:45:00 +0800

    左半邊區域切版

--- components/Clock/index.js
+++ components/Clock/index.js
@@ -3,13 +3,46 @@ import styles from "./styles/index.module.sass";
 export default function Indexs() {
   return (
     <div className={styles["clock-wrap"]}>
-      <div className={styles["logo-bg"]}>
-        <h1>PODOMORO</h1>
+      <div style={{ display: "none" }}>
+        <div className={styles["logo-bg"]}>
+          <h1>PODOMORO</h1>
+        </div>
+        <div className={styles["content-text"]}>
+          You don't have any task now. <br />
+          please add task first!
+        </div>
       </div>
 
-      <div className={styles["content-text"]}>
-        You don't have any task now. <br />
-        please add task first!
+      <div className={styles["clock-info"]}>
+        <div className={styles["task-name"]}>My First Task</div>
+        <div>circle</div>
+        <div className={styles["btn-wrap"]}>
+          <button>
+            <Image
+              src="/images/icons/start_gray.svg"
+              alt="開始"
+              width={60}
+              height={60}
+            />
+          </button>
+          <button>
+            <Image
+              src="/images/icons/stop_gray.svg"
+              alt="暫停"
+              width={50}
+              height={50}
+            />
+          </button>
+          <button>
+            <Image
+              src="/images/icons/reset_gray.svg"
+              alt="重新"
+              width={50}
+              height={50}
+            />
+          </button>
+        </div>
+        <div className={styles["task-note"]}>TASK COMPLETE</div>
       </div>
       <div className={styles["footer-text"]}>PODOMORO</div>
     </div>
