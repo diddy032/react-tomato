279065b6634ca9d91e3820770e5cfc20df8f07f9
diddy032 <gwul3vu032@gmail.com>
2021-03-11 00:15:52 +0800

    AddTask  add function

--- components/AddTask/index.js
+++ components/AddTask/index.js
@@ -1,15 +1,49 @@
+import { useState, useEffect } from "react";
 import NavbarContainer from "../Shared/NavbarContainer";
 import EstimatedTomato from "../../components/Shared/EstimatedTomato";
 import styles from "./styles/index.module.sass";
 export default function Indexs() {
+  const [taskData, setTaskData] = useState([]);
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskTime, setTaskTime] = useState(0);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("TaskDataArray"));
+    if (data) {
+      setTaskData(data);
+    } else {
+      setTaskData([]);
+    }
+  }, []);
+
+  const handleSaveTaskData = () => {
+    let saveObj = {
+      title: taskTitle,
+      time: taskTime,
+    };
+    taskData.push(saveObj);
+    console.log("taskData:", taskData, "\nsaveObj", saveObj);
+    localStorage.setItem("TaskDataArray", JSON.stringify(taskData));
+  };
+
   return (
     <NavbarContainer title={"ADD NEW TASK"}>
       <div className={styles["content"]}>
         <label htmlFor="input-task">TASK TITLE</label>
-        <input id="input-task" type="text" placeholder="My Task" />
+        <input
+          id="input-task"
+          type="text"
+          placeholder="My Task"
+          onChange={(e) => setTaskTitle(e.target.value)}
+        />
         <EstimatedTomato />
       </div>
-      <button className={styles["add-btn"]}>ADD TASK</button>
+      <button
+        className={styles["add-btn"]}
+        onClick={() => handleSaveTaskData()}
+      >
+        ADD TASK
+      </button>
     </NavbarContainer>
   );
 }
