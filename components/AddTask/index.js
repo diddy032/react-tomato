c870736c91803b69509f4ace83d4c7766299c573
diddy032 <gwul3vu032@gmail.com>
2021-03-16 00:22:04 +0800

    estimated tomato 功能

--- components/AddTask/index.js
+++ components/AddTask/index.js
@@ -5,24 +5,30 @@ import styles from "./styles/index.module.sass";
 export default function Indexs() {
   const [taskData, setTaskData] = useState([]);
   const [taskTitle, setTaskTitle] = useState("");
-  const [taskTime, setTaskTime] = useState(0);
+  const [taskTime, setTaskTime] = useState(1);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("TaskDataArray"));
-    if (data) {
-      setTaskData(data);
-    } else {
-      setTaskData([]);
-    }
+    data ? setTaskData(data) : setTaskData([]);
   }, []);
 
   const handleSaveTaskData = () => {
+    if (taskTitle.length === 0) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+    }
+
     let saveObj = {
       title: taskTitle,
       time: taskTime,
     };
     taskData.push(saveObj);
-    console.log("taskData:", taskData, "\nsaveObj", saveObj);
+    setTaskTitle("");
+    setTaskTime(1);
+    // console.log("taskData:", taskData, "\nsaveObj", saveObj);
     localStorage.setItem("TaskDataArray", JSON.stringify(taskData));
   };
 
@@ -34,9 +40,11 @@ export default function Indexs() {
           id="input-task"
           type="text"
           placeholder="My Task"
+          value={taskTitle}
           onChange={(e) => setTaskTitle(e.target.value)}
         />
-        <EstimatedTomato />
+        {error && <div>此為必填欄位</div>}
+        <EstimatedTomato taskTime={taskTime} setTaskTime={setTaskTime} />
       </div>
       <button
         className={styles["add-btn"]}
--- components/Shared/EstimatedTomato.js
+++ components/Shared/EstimatedTomato.js
@@ -1,19 +1,29 @@
-import styles from './styles/estimatedTomato.module.sass'
-export default function EstimatedTomato(props) {
+import { useState, useEffect } from "react";
+import styles from "./styles/estimatedTomato.module.sass";
+export default function EstimatedTomato({ taskTime, setTaskTime }) {
+  const totalTime = 8;
+  const [tomatoArr, setTomatoArr] = useState([]);
+  useEffect(() => {
+    function tomatoList() {
+      let arr = [];
+      for (let i = 1; i < totalTime + 1; i++) {
+        i <= taskTime ? arr.push(true) : arr.push(false);
+      }
+      setTomatoArr(arr);
+    }
+    tomatoList();
+  }, [taskTime]);
   return (
     <>
-      <div className={styles['list-title']}>ESTIMATED TOMATO</div>
+      <div className={styles["list-title"]}>ESTIMATED TOMATO</div>
       <div className={styles["tomato-list"]}>
-        <img src="/images/icons/tomato_small_color.svg" alt="" />
-        <img src="/images/icons/tomato_small_color.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
-        <img src="/images/icons/tomato_small_gray.svg" alt="" />
+        {tomatoArr.map((item, index) => (
+          <button
+            className={styles[item ? "active" : "origin"]}
+            key={index}
+            onClick={() => setTaskTime(index + 1)}
+          />
+        ))}
       </div>
     </>
   );
