3dca8289a8cd55904ccaeb0781a7220c43c8f152
diddy032 <gwul3vu032@gmail.com>
2021-04-18 15:58:51 +0800

    countdown timer

--- components/Clock/index.js
+++ components/Clock/index.js
@@ -1,32 +1,101 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import styles from "./styles/index.module.sass";
 export default function Indexs({ data, saveData }) {
+  const activeMins = 25;
   const [activeItem, setActive] = useState({});
+  const [lastSecs, setLastSecs] = useState(activeMins * 60);
+  const [isStart, setIsStart] = useState(false);
+
+  const countRef = useRef(null);
+
   useEffect(() => {
     if (data.length === 0) return;
     let item = data.filter((e) => e.IsArchive);
     item.length > 0 ? setActive(item[0]) : setActive(data[0]);
   }, [data]);
+
+  useEffect(() => {
+    if (lastSecs < 1) {
+      clearInterval(countRef.current);
+      setIsStart(false);
+    }
+  }, [lastSecs]);
+
   console.log("時鐘：", "\ndata:", data);
   console.log("activeItem:", activeItem);
+
+  const handleStardTime = () => {
+    if (isStart === true) {
+      // clearInterval(countRef.current);
+      // setIsStart(false);
+      stopTimer();
+    }
+    // Else, lastSecs is inactive => START
+    else if (lastSecs > 1) {
+      setIsStart(true);
+      countRef.current = setInterval(() => {
+        setLastSecs((lastSecs) => lastSecs - 1);
+      }, 1000);
+    }
+  };
+
+  const handleReset = () => {
+    stopTimer();
+    // clearInterval(countRef.current);
+    // setIsStart(false);
+    setLastSecs(activeMins * 60);
+  };
+
+  const stopTimer = () => {
+    clearInterval(countRef.current);
+    setIsStart(false);
+  };
+
+  const handleDecrease = () => {
+    setLastSecs(lastSecs - 60);
+  };
+
+  const handleIncrement = () => {
+    setLastSecs(lastSecs + 60);
+  };
+
+  const formatTime = () => {
+    const getSeconds = `0${lastSecs % 60}`.slice(-2);
+    const minutes = `${Math.floor(lastSecs / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+
+    return `${getMinutes} : ${getSeconds}`;
+  };
 
   return (
     <div className={styles["clock-wrap"]}>
       {activeItem ? (
         <div className={styles["clock-info"]}>
           <div className={styles["task-name"]}>
-            {activeItem?.TaskName ?? "-"}
+            {/* {activeItem?.TaskName ?? "-"} */}
+            {formatTime()}
           </div>
           <div>circle</div>
           <div className={styles["btn-wrap"]}>
-            <button className={styles["icon-start"]}>
+            <button
+              className={styles["icon-start"]}
+              onClick={() => handleStardTime()}
+              disabled={isStart}
+            >
               <span>START</span>
             </button>
-            <button className={styles["icon-stop"]}>
+            <button
+              className={styles["icon-stop"]}
+              onClick={() => handleStardTime()}
+              disabled={!isStart}
+            >
               <span>STOP</span>
             </button>
-            <button className={styles["icon-reset"]}>
+            <button
+              className={styles["icon-reset"]}
+              onClick={() => handleReset()}
+            >
               <span>RESET</span>
             </button>
           </div>
